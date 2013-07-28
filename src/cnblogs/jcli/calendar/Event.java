1166d65dbaa565aac8fee687c1da51bb9771262d
lihongfu <tony.li.fly@gmail.com>
2013-07-28 19:22:44 +0800

    change Event define

--- src/cnblogs/jcli/calendar/Event.java
+++ src/cnblogs/jcli/calendar/Event.java
@@ -2,7 +2,38 @@ package cnblogs.jcli.calendar;
 
 import java.util.Date;
 
+/**
+ * 日历事件/活动.
+ * 
+ *
+ * @author tony.li.fly@gmail.com
+ */
 public class Event {
+	/**
+	 * 事件标题
+	 */
+	private String title;
+	
+	/**
+	 * 事件描述
+	 */
+	private String description;
+	
+	/**
+	 * 事件发生地点
+	 */
+	private String location;
+	
+	 /**
+     * 事件发生的开始时间
+     */
+    private Date startDate;
+    
+    /**
+     * 事件发生的结束时间
+     */
+    private Date endDate;
+	
     /**
      * 日历类型: 1,公历 2,农历
      */
@@ -13,15 +44,6 @@ public class Event {
      */
     private String rule;
     
-    /**
-     * 事件发生的开始时间
-     */
-    private Date startDate;
-    
-    /**
-     * 事件发生的结束时间
-     */
-    private Date endDate;
     
     public int getCalendarType() {
         return calendarType;
@@ -55,4 +77,29 @@ public class Event {
         this.endDate = endDate;
     }
 
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getLocation() {
+		return location;
+	}
+
+	public void setLocation(String location) {
+		this.location = location;
+	}
+
+    
 }
